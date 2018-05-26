7fedc29cd66e68eb26a47c24a438398743244bdc
Sumner Rittby <sumner@crunchyintheory.com>
2018-05-26 10:47:50 -0500

    Refactored tagme and untagme to allow tagging of other members. Added on leave tags to both.

--- src/commands/Core/TagMe.js
+++ src/commands/Core/TagMe.js
@@ -13,35 +13,66 @@ class TagMe extends Command {
 
         this.allowDM = false;
 
-        this.regex = new RegExp('^(?:tagme|tag) ?(.+)?$');
+        this.regex = new RegExp('^(?:tagme|tag) ?(.*?)?(?: <@!?(\\d+)>)?$');
     }
 
     run(message) {
-        let tag = message.strippedContent.match(this.regex, 'i')[1];
+        let match = message.strippedContent.match(this.regex, 'i');
+        let tag = match[1];
+        let target = match[2];
+
+        let argstr = ' Correct format is \'!tagme (overwatch/warframe/sk/bot/rank/leave)\'';
         if (tag === undefined) {
-            message.channel.send('Error: Argument blank. Correct format is \'!tagme (overwatch/warframe/sk/bot/rank)\''); return;
-        } else if (!tag.match(/^(?:overwatch|warframe|sk|bot|rank)$/i)) {
-            message.channel.send('Error: Argument invalid. Correct format is \'!tagme (overwatch/warframe/sk/bot/rank)\''); return;
+            message.channel.send(`Error: Argument blank.${argstr}`); return;
+        } else if (!tag.match(/^(?:overwatch|warframe|sk|bot|rank|(?:on)?leave)$/i)) {
+            message.channel.send(`Error: Argument invalid.${argstr}`); return;
         }
         tag = tag.toLowerCase();
-        
-        if (tag === 'rank') {
-            this.bot.settings.getMember(message.author.id).then((member) => {
-                DiscordTags.assignRankTagsToMember(message, member.Rank);
+
+        function callback() {
+            if (tag === 'rank') {
+                this.bot.settings.getMember(target.id).then((member) => {
+                    DiscordTags.assignRankTagsToMember(message, member.Rank, target);
+                });
+            }
+            else {
+                /*eslint-disable indent*/
+                switch (tag) {
+                    case 'overwatch': DiscordTags.addRoleToMember(target, 'Overwatch'); break;
+                    case 'warframe': DiscordTags.addRoleToMember(target, 'Warframe'); break;
+                    case 'sk': DiscordTags.addRoleToMember(target, 'Spiral Knights'); break;
+                    case 'bot': DiscordTags.addRoleToMember(target, 'Bot Notifications'); break;
+                    case 'leave': this.addOnLeaveTag(target); break;
+                    case 'onleave': this.addOnLeaveTag(target); break;
+                }
+                /*eslint-enable indent*/
+            }
+            message.channel.send(`Successfully gave ${target.id == message.author.id ? 'you' : target.username} the ${tag} tag!`);
+            this.bot.logger.info(`Added ${tag} tag to member ${target.username}`);
+        }
+        if (target === undefined) {
+            target = message.member;
+            callback.bind(this)();
+        } else {
+            message.guild.fetchMember(target).then((m) => {
+                target = m;
+                callback.bind(this)();
             });
         }
-        else {
-            /*eslint-disable indent*/
-            switch (tag) {
-                case 'overwatch': DiscordTags.addRoleToMember(message.member, 'Overwatch'); break;
-                case 'warframe': DiscordTags.addRoleToMember(message.member, 'Warframe'); break;
-                case 'sk': DiscordTags.addRoleToMember(message.member, 'Spiral Knights'); break;
-                case 'bot': DiscordTags.addRoleToMember(message.member, 'Bot Notifications'); break;
+    }
+
+    addOnLeaveTag(member) {
+        this.bot.settings.getMember(member.id).then((dbmember) => {
+            if (dbmember.Rank == 7) {
+                return DiscordTags.addRoleToMember(member, 'On Leave (GM)');
             }
-            /*eslint-enable indent*/
-        }
-        message.channel.send(`Successfully gave you the ${tag} tag!`);
-        this.bot.logger.info(`Added ${tag} tag to member ${message.author.username}`);
+            else if (dbmember.Rank >= 5) {
+                return DiscordTags.addRoleToMember(member, 'On Leave (General)');
+            }
+            else {
+                return DiscordTags.addRoleToMember(member, 'On Leave (Member)');
+            }
+        });
     }
 }
 
--- src/commands/Core/UnTagMe.js
+++ src/commands/Core/UnTagMe.js
@@ -13,27 +13,58 @@ class UnTagMe extends Command {
 
         this.allowDM = false;
 
-        this.regex = new RegExp('^(?:untagme|untag) ?(.+)?$');
+        this.regex = new RegExp('^(?:untagme|untag) ?(.*?)?(?: <@!?(\\d+)>)?$');
     }
 
     run(message) {
-        let tag = message.strippedContent.match(this.regex, 'i')[1];
+        let match = message.strippedContent.match(this.regex, 'i');
+        let tag = match[1];
+        let target = match[2];
+
+        let argstr = ' Correct format is \'!tagme (overwatch/warframe/sk/bot/leave)\'';
         if (tag === undefined) {
-            message.channel.send('Error: Argument blank. Correct format is \'!tagme (overwatch/warframe/sk/bot)\''); return;
-        } else if (!tag.match(/^(?:overwatch|warframe|sk|bot)$/i)) {
-            message.channel.send('Error: Argument invalid. Correct format is \'!tagme (overwatch/warframe/sk/bot)\''); return;
+            message.channel.send(`Error: Argument blank.${argstr}`); return;
+        } else if (!tag.match(/^(?:overwatch|warframe|sk|bot|(?:on)?leave)$/i)) {
+            message.channel.send(`Error: Argument invalid.${argstr}`); return;
         }
         tag = tag.toLowerCase();
-        /*eslint-disable indent*/
-        switch (tag) {
-            case 'overwatch': DiscordTags.removeRoleFromMember(message.member, 'Overwatch'); break;
-            case 'warframe': DiscordTags.removeRoleFromMember(message.member, 'Warframe'); break;
-            case 'sk': DiscordTags.removeRoleFromMember(message.member, 'Spiral Knights'); break;
-            case 'bot': DiscordTags.removeRoleFromMember(message.member, 'Bot Notifications'); break;
+        function callback() {
+            /*eslint-disable indent*/
+            switch (tag) {
+                case 'overwatch': DiscordTags.removeRoleFromMember(target, 'Overwatch'); break;
+                case 'warframe': DiscordTags.removeRoleFromMember(target, 'Warframe'); break;
+                case 'sk': DiscordTags.removeRoleFromMember(target, 'Spiral Knights'); break;
+                case 'bot': DiscordTags.removeRoleFromMember(target, 'Bot Notifications'); break;
+                case 'leave': this.removeOnLeaveTag(target); break;
+                case 'onleave': this.removeOnLeaveTag(target); break;
+            }
+            /*eslint-enable indent*/
+            message.channel.send(`Successfully removed the ${tag} tag!`);
+            this.bot.logger.info(`Removed ${tag} tag from member ${target.username}`);
         }
-        /*eslint-enable indent*/
-        message.channel.send(`Successfully removed the ${tag} tag!`);
-        this.bot.logger.info(`Removed ${tag} tag from member ${message.author.username}`);
+        if (target === undefined) {
+            target = message.member;
+            callback.bind(this)();
+        } else {
+            message.guild.fetchMember(target).then((m) => {
+                target = m;
+                callback.bind(this)();
+            });
+        }
+    }
+
+    removeOnLeaveTag(member) {
+        this.bot.settings.getMember(member.id).then((dbmember) => {
+            if (dbmember.Rank == 7) {
+                return DiscordTags.removeRoleFromMember(member, 'On Leave (GM)');
+            }
+            else if (dbmember.Rank >= 5) {
+                return DiscordTags.removeRoleFromMember(member, 'On Leave (General)');
+            }
+            else {
+                return DiscordTags.removeRoleFromMember(member, 'On Leave (Member)');
+            }
+        });
     }
 }
 
