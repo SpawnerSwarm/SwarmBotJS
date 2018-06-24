f7a668da1a2a3d08de6cd7f53fec5ccfe5112cff
Sumner Rittby <sumner@crunchyintheory.com>
2018-06-24 01:19:35 -0500

    tsify UserEmotes.js

--- src/objects/UserEmotes.on.ts
+++ src/objects/UserEmotes.on.ts
@@ -1,33 +1,52 @@
-/*eslint-disable quotes*/
-module.exports = [
+import { Snowflake } from "discord.js";
+
+export function simple(emote: UserEmote): emote is SimpleUserEmote {
+    return emote.isSimple;
+}
+
+export interface UserEmote {
+    isSimple: boolean,
+    call: string
+}
+
+export interface SimpleUserEmote extends UserEmote {
+    user: Snowflake,
+    content: string
+}
+
+export interface ComplexUserEmote extends UserEmote {
+    function(id: Snowflake): Promise<string>;
+}
+
+export default [
     {
         isSimple: true,
         user: "139109512744402944",
         content: "http://i.imgur.com/ACoUhAW.gifv",
         call: "holdup"
-    },
+    } as SimpleUserEmote,
     {
         isSimple: true,
         user: "138043934143152128",
         content: "http://i.imgur.com/1xzQkSo.png",
         call: "brutal"
-    },
+    } as SimpleUserEmote,
     {
         isSimple: true,
         user: "137976237292388353",
         content: "test",
         call: "etest"
-    },
+    } as SimpleUserEmote,
     {
         isSimple: true,
         user: "138043934143152128",
         content: "Funks go e y y man kill zucchini angst",
         call: "gecko"
-    },
+    } as SimpleUserEmote,
     {
         isSimple: false,
-        function: (id) => {
-            return new Promise((resolve) => {
+        function: (id: Snowflake) => {
+            return new Promise<string>((resolve) => {
                 /*eslint-disable indent*/
                 switch(id) {
                     case "138043934143152128": resolve("http://i.imgur.com/MXeL1Jh.gifv"); break; //Fox
@@ -39,5 +58,5 @@ module.exports = [
             });
         },
         call: "lenny"
-    }
-];
+    } as ComplexUserEmote
+] as UserEmote[];
