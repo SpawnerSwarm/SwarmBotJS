033a26a7b26925f26681958167e79ff9755caf95
Sumner Rittby <sumner@crunchyintheory.com>
2018-06-25 22:11:21 -0500

    tsify Info.js

--- src/commands/Core/Info.ts
+++ src/commands/Core/Info.ts
@@ -1,15 +1,12 @@
-'use strict';
-
-const Command = require('../../Command.js');
-const os = require('os');
-const exec = require('child_process').exec;
-const Discord = require('discord.js');
+import Command from "../../objects/Command";
+import * as os from "os";
+import { exec } from "child_process";
+import { version } from "discord.js";
+import Cephalon from "../../Cephalon";
+import { MessageWithStrippedContent } from "../../objects/Types";
 
 class Info extends Command {
-    /**
-     * @param {Cephalon} bot
-     */
-    constructor(bot) {
+    constructor(bot: Cephalon) {
         super(bot, 'core.debug.info', 'info', 'Returns info about the current instance.');
 
         this.regex = new RegExp('^(?:info|debug|session|vars)$');
@@ -17,17 +14,17 @@ class Info extends Command {
         this.requiredRank = 0;
     }
 
-    run(message) {
-        this.bot.settings.getVersion().then((mySQLVersion) => {
+    run(message: MessageWithStrippedContent) {
+        this.bot.db.getVersion().then((mySQLVersion: string) => {
             new Promise((resolve) => {
                 exec('git rev-parse --short=7 HEAD', (error, stdout) => {
                     resolve(stdout);
                 });
-            }).then((gitHash) => {
+            }).then((gitHash: string) => {
                 let str = `Platform: ${os.type()}${process.arch}`;
                 str += `\nCPUs: ${os.cpus().length}`;
                 str += `\nBot version: ${gitHash}`;
-                str += `\nDiscord.js version: ${Discord.version}`;
+                str += `\nDiscord.js version: ${version}`;
                 str += `\n\nNode.js version: ${process.versions.node}`;
                 str += `\nV8 version: ${process.versions.v8}`;
                 str += `\n\nMySQL Version: ${mySQLVersion}`;
