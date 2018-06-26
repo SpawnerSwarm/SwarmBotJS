5f1ab31cd79363b8b1a7ebd8de9991587bcdb763
Sumner Rittby <sumner@crunchyintheory.com>
2018-06-25 22:04:15 -0500

    tsify Help.js

--- src/commands/Core/Help.ts
+++ src/commands/Core/Help.ts
@@ -1,10 +1,10 @@
-'use strict';
+import Command from "../../objects/Command";
+import * as fs from "fs";
+import Cephalon from "../../Cephalon";
+import { MessageWithStrippedContent } from "../../objects/Types";
 
-const Command = require('../../Command.js');
-const fs = require('fs');
-
-class Help extends Command {
-    constructor(bot) {
+export default class Help extends Command {
+    constructor(bot: Cephalon) {
         super(bot, 'core.help', 'help');
 
         this.allowDM = true;
@@ -14,10 +14,11 @@ class Help extends Command {
         this.regex = /(?:help|man(?:ual)?|docs?|what)(?: (.+))?/i;
     }
 
-    run(message) {
+    run(message: MessageWithStrippedContent) {
         let match = message.strippedContent.match(this.regex);
+        if(!this._tsoverrideregex(match)) return;
         this.bot.logger.debug(match[1]);
-        let name;
+        let name: string;
         
         if(!match[1]) name = '_';
         else name = match[1].toLowerCase();
@@ -25,7 +26,7 @@ class Help extends Command {
         if (name == 'about-this-server') return;
         if (fs.existsSync(`./docs/${name}.md`)) {
             try {
-                fs.readFile(`./docs/${name}.md`, function (err, data) {
+                fs.readFile(`./docs/${name}.md`, function (err: NodeJS.ErrnoException, data: Buffer) {
                     if (err) throw err;
                     this.react('✅');
                     let str = data.toString();
@@ -45,5 +46,3 @@ class Help extends Command {
         }
     }
 }
-
-module.exports = Help;
