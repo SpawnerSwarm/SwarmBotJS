7d3a0af037751f8d74c5ad2a44a2d3bde7496dd4
Sumner Rittby <sumner@crunchyintheory.com>
2018-06-26 19:41:34 -0500

    tsify GetMember.js

--- src/commands/Database/GetMember.ts
+++ src/commands/Database/GetMember.ts
@@ -1,14 +1,12 @@
-﻿'use strict';
+﻿import Command from "../../objects/Command";
+import Ranks from "../../objects/Ranks.on";
+import Cephalon from "../../Cephalon";
+import { MessageWithStrippedContent } from "../../objects/Types";
+import Member from "../../objects/Member";
+import { User, RichEmbed, RichEmbedOptions } from "discord.js";
 
-const Command = require('../../Command.js');
-
-const Ranks = require('../../resources/Ranks.js');
-
-class GetMember extends Command {
-    /**
-     * @param {Cephalon} bot
-    */
-    constructor(bot) {
+export default class GetMember extends Command {
+    constructor(bot: Cephalon) {
         super(bot, 'database.getMember', 'getMember', 'Get information about a specified member');
 
         this.usages = [
@@ -16,33 +14,34 @@ class GetMember extends Command {
         ];
 
         //this.regex = new RegExp('^(?:getMember|member(?: get)?)? <@!?(\d+)> ?(.+)?$', 'i');
-        this.regex = new RegExp('^(?:getMember|member(?: get)?) <@!?(\\d+)> ?(.+)?$', 'i');
+        this.regex = /^(?:getMember|member(?: get)?) <@!?(\\d+)> ?(.+)?$/i;
 
         this.allowDM = false;
 
         this.requiredRank = 0;
     }
 
-    run(message) {
-        const messageMatch = message.strippedContent.match(this.regex, 'i');
+    run(message: MessageWithStrippedContent) {
+        const messageMatch = message.strippedContent.match(this.regex);
+        if(!this._tsoverrideregex(messageMatch)) return;
         let verbose = messageMatch[2];
         if (!messageMatch[1]) {
             message.channel.send('Syntax incorrect');
         } else {
-            /**
-             * @type {Boolean}
-             */
             let iVerbose = verbose === '-v' || verbose === '--verbose';
-            this.bot.settings.getMember(messageMatch[1]).then((member) => {
-                this.bot.client.fetchUser(messageMatch[1]).then((user) => {
-                    var embed = {
-                        type: 'rich',
+            this.bot.db.getMember(messageMatch[1]).then((member: Member) => {
+                this.bot.client.fetchUser(messageMatch[1]).then((user: User) => {
+                    type Field = {
+                        name: string,
+                        value: string,
+                        inline: boolean
+                    };
+                    var embedOpts: RichEmbedOptions = {
                         description: 'foo',
                         author: {
                             icon_url: user.avatarURL,
                             name: member.Name,
                         },
-                        color: Ranks[member.Rank].color,
                         fields: [
                             {
                                 name: 'Rank',
@@ -53,93 +52,79 @@ class GetMember extends Command {
                                 name: 'Last Rankup',
                                 value: '...',
                                 inline: true,
-                            },
-                            {
-                                name: 'Forma Donated',
-                                value: member.FormaDonated,
-                                inline: true,
-                            },
+                            }
                         ],
                     };
                     if (iVerbose) {
                         if (member.WarframeName) {
-                            embed.fields.push({
+                            (embedOpts.fields as Field[]).push({
                                 name: 'Warframe',
                                 value: member.WarframeName,
                                 inline: true,
                             });
                         }
                         if (member.SpiralKnightsName) {
-                            embed.fields.push({
+                            (embedOpts.fields as Field[]).push({
                                 name: 'Spiral Knights',
                                 value: member.SpiralKnightsName,
                                 inline: true,
                             });
                         }
-                        if (member.SteamName) {
-                            embed.fields.push({
-                                name: 'Steam',
-                                value: member.SteamName,
-                                inline: true,
-                            });
-                        }
                     }
                     if(member.Banned) {
-                        embed.fields.push({
+                        (embedOpts.fields as Field[]).push({
                             name: 'BANNED',
                             value: 'BANNED',
                             inline: member.Ally,
                         });
                     }
                     if(member.Ally) {
-                        embed.fields.push({
+                        (embedOpts.fields as Field[]).push({
                             name: 'ALLY',
                             value: 'ALLY',
                             inline: member.Banned
                         });
                     }
-                    new Promise((resolve) => {
+                    let embed = new RichEmbed(embedOpts);
+                    let color = Ranks[member.Rank].color;
+                    if(color !== undefined) embed.setColor(color);
+
+                    new Promise<null | string>((resolve) => {
                         let rank = Ranks[member.Rank].name;
                         if (member[rank] === null) {
                             resolve(null);
                         }
-                        embed.fields[1].value = new Date(member[rank]).toDateString();
+                        (embed.fields as Field[])[1].value = new Date(member[rank]).toDateString();
                         resolve(member[rank]);
                     }).then((date) => this.checkReadyForRankup(date, member, embed))
-                        .then(() => message.channel.send('', {embed: embed}));
+                        .then(() => message.channel.send('', { embed: embed }));
                 });
             })
             .catch((err) => {
-                this.bot.logger.error(err);
+                this.logger.error(err);
                 message.channel.send(`\`Error: ${err}\``);
             });
         }
     }
 
-    /**
-     * @param {string} date
-     * @param {Object} member
-     * @param {MessageEmbed} embed
-     */
-    checkReadyForRankup(date, member, embed) {
-        if (date === null) {
+    checkReadyForRankup(dateStr: string | null, member: Member, embed: RichEmbed) {
+        if (dateStr === null) {
             embed.description = `Rankup data not found for ${member.Name}`; return;
         }
         let one = 1000 * 60 * 60 * 24;
         let today = new Date();
         
-        date = new Date(date);
+        let date = new Date(dateStr);
         
-        let dateDiff = Math.floor((today - date) / one);
+        let dateDiff = Math.floor((today.valueOf() - date.valueOf()) / one);
+        let last = Ranks[member.Rank].last;
         if (member.Rank === 7) {
             embed.description = 'He/She has reached the maximum possible rank.';
         }
-        else if (dateDiff >= Ranks[member.Rank].last) {
+        else if (last === undefined || dateDiff >= last) {
             embed.description = `${member.Name} is eligible for a rankup.\nIt has been ${dateDiff} days since their last test`;
         } else {
-            embed.description = `${member.Name} is not eligible for a rankup.\nIt has been ${dateDiff} days since their last test and they have ${Ranks[member.Rank].last - dateDiff} days to go.`;
+            embed.description = `${member.Name} is not eligible for a rankup.\nIt has been ${dateDiff} days since their last test and they have ${last - dateDiff} days to go.`;
         }
     }
 }
-
-module.exports = GetMember;
---
+++ src/helpers/DiscordTags.ts
@@ -0,0 +1,34 @@
+﻿import Ranks from "../objects/Ranks.on";
+import { Message, GuildMember } from "discord.js";
+import { RankNum } from "../objects/Types";
+
+export default class DiscordTags {
+    static assignRankTagsToMember(message: Message, rank: RankNum, discordMember: GuildMember): void {
+        let member: GuildMember = message.member;
+        if(discordMember) {
+            member = discordMember;
+        }
+        new Promise((resolve) => {
+            for (let i = 1; i <= rank; i++) {
+                DiscordTags.addRoleToMember(member, Ranks[i].name);
+            }
+            resolve();
+        }).then(() => {
+            for (let i = 7; i > rank; i--) {
+                DiscordTags.removeRoleFromMember(member, Ranks[i].name);
+            }
+        });
+    }
+
+    static addRoleToMember(member: GuildMember, roleName: string): void {
+        let role = member.guild.roles.find('name', roleName);
+
+        member.addRole(role);
+    }
+
+    static removeRoleFromMember(member: GuildMember, roleName: string): void {
+        let role = member.guild.roles.find('name', roleName);
+
+        member.removeRole(role);
+    }
+}
