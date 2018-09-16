25b7416891d34a6d442bffde0f867ca7b9be8655
Sumner Rittby <sumner@crunchyintheory.com>
2018-09-16 00:10:36 -0500

    Update Info to modern node async

--- src/commands/Core/Info.ts
+++ src/commands/Core/Info.ts
@@ -1,7 +1,7 @@
 import Command from "../../objects/Command";
 import * as os from "os";
 import { exec } from "child_process";
-import { version } from "discord.js";
+import { version as discordVersion } from "discord.js";
 import Cephalon from "../../Cephalon";
 import { MessageWithStrippedContent } from "../../objects/Types";
 
@@ -14,26 +14,20 @@ export default class Info extends Command {
         this.requiredRank = 0;
     }
 
-    run(message: MessageWithStrippedContent) {
-        this.bot.db.getVersion().then((mySQLVersion: string) => {
-            new Promise((resolve) => {
-                exec('git rev-parse --short=7 HEAD', (error, stdout) => {
-                    resolve(stdout);
-                });
-            }).then((gitHash: string) => {
-                let str = `Platform: ${os.type()}${process.arch}`;
-                str += `\nCPUs: ${os.cpus().length}`;
-                str += `\nBot version: ${process.env.VERSIONS}`;
-                str += `\nDiscord.js version: ${version}`;
-                str += `\n\nNode.js version: ${process.versions.node}`;
-                str += `\nV8 version: ${process.versions.v8}`;
-                str += `\n\nMySQL Version: ${mySQLVersion}`;
-                str += `\nMySQL Host: ${process.env.MYSQL_HOST}`;
-                str += `\nMySQL Port: ${process.env.MYSQL_PORT}`;
-                str += `\nMySQL Database: ${process.env.MYSQL_DB}`;
-
-                message.channel.send(str);
-            });
-        });
+    async run(message: MessageWithStrippedContent) {
+        const mySQLVersion: string = await this.bot.db.getVersion();
+        const version: string = require('../../package.json').version;
+        let str = `Platform: ${os.type()}${process.arch}`;
+        str += `\nCPUs: ${os.cpus().length}`;
+        str += `\nBot version: ${version}`;
+        str += `\nDiscord.js version: ${discordVersion}`;
+        str += `\n\nNode.js version: ${process.versions.node}`;
+        str += `\nV8 version: ${process.versions.v8}`;
+        str += `\n\nMySQL Version: ${mySQLVersion}`;
+        str += `\nMySQL Host: ${process.env.MYSQL_HOST}`;
+        str += `\nMySQL Port: ${process.env.MYSQL_PORT}`;
+        str += `\nMySQL Database: ${process.env.MYSQL_DB}`;
+        await message.channel.send(str);
+        return true;
     }
 }
